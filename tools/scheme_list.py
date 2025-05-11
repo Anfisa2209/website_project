c737f7e0b93e0f7df2b6e51d28469400ec2d9127
Anfisa <anfis.igorevna@gmail.com>
2025-05-11 11:14:11 +0300

    контакты

--- tools/scheme_list.py
+++ tools/scheme_list.py
@@ -1,6 +1,4 @@
-from tools.service_files import return_files
-
-SCHEME_LIST = [i.split("/")[-1].split('.')[0] for i in return_files('./static/img/scheme')]
+SCHEME_LIST = ['A', 'AF', 'C', 'CF', 'E', 'EF', 'G', 'L']
 VIDEO_LIST = {"A": 'https://www.youtube.com/embed/JQGyltY-ncY?si=Oh_7-Fw0rs6aXW5z',
               "C": 'https://www.youtube.com/embed/sIxVlZvt-ug?si=R4CjzOwWVJDcEgeD',
               "G": 'https://www.youtube.com/embed/yQgvUCz8vWc?si=vWPCL0cwgXKgaBz4',
